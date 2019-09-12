3bf87482c988cfc14497f1be5186a8ababdac744
suzennn <52767681+suzennn@users.noreply.github.com>
2019-09-12 18:53:08 +0800

    fixed layout of login

--- lib/start/login.dart
+++ lib/start/login.dart
@@ -5,7 +5,7 @@ import '../main.dart';
 class LoginPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Scaffold(
+    return Scaffold(    
       backgroundColor: Theme.of(context).backgroundColor, 
       body: LoginForm()
     );
