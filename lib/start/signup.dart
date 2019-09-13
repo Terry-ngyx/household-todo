e3c9d3828ca497dae0e680c27586e7d3a1fe8efb
Terrence Ng <terrence_ngyx@hotmail.com>
2019-09-13 15:43:15 +0800

    sign up can get the response!

--- lib/start/signup.dart
+++ lib/start/signup.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
 import '../main.dart';
 import '../style.dart';
 
@@ -17,12 +22,54 @@ class SigninForm extends StatefulWidget {
   _SigninFormState createState() => _SigninFormState();
 }
 
+class User {
+  String status;
+
+  User({this.status});
+
+  factory User.fromJson(Map<String, dynamic> parsedJson) {
+    return User(
+      status: parsedJson['status'],
+    );
+  }
+}
+
 class _SigninFormState extends State<SigninForm> {
   final _formkey = GlobalKey<FormState>();
   String username;
   String email;
   String password;
   String confirmPassword;
+
+  _signup(String userName, String userUsername, String userEmail,
+      String userPassword, String userConfirmPassword) async {
+    // set up POST request arguments
+    String url = 'http://10.0.2.2:5000/api/v1/users/signup';
+    Map<String, String> headers = {"Content-type": "application/json"};
+    String json =
+        '{"name": "$userName", "username": "$userUsername", "email": "$userEmail", "password": "$userPassword", "confirmed_password": "$userConfirmPassword"}';
+    print(json);
+    // make POST request
+    http.Response response = await http.post(url, headers: headers, body: json);
+    print(response);
+    // check the status code for the result
+    int statusCode = response.statusCode;
+    print(statusCode);
+    // this API passes back the id of the new item added to the body
+
+    final jsonResponse = jsonDecode(response.body);
+    User user = new User.fromJson(jsonResponse);
+    print(user.status);
+
+    // if (body[status]==){
+    // }
+    // {
+    //   "title": "Hello",
+    //   "body": "body text",
+    //   "userId": 1,
+    //   "id": 101
+    // }
+  }
 
   final passwordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
@@ -59,13 +106,13 @@ class _SigninFormState extends State<SigninForm> {
                             child: TextFormField(
                               decoration: InputDecoration(
                                 enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Color(0xFFF96861)),
+                                  borderSide:
+                                      BorderSide(color: Color(0xFFF96861)),
                                   borderRadius: BorderRadius.circular(15.0),
                                 ),
                                 focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white),
-                                  borderRadius: BorderRadius.circular(15.0)
-                                ),
+                                    borderSide: BorderSide(color: Colors.white),
+                                    borderRadius: BorderRadius.circular(15.0)),
                               ),
                               autofocus: false,
                               obscureText: false,
@@ -87,7 +134,8 @@ class _SigninFormState extends State<SigninForm> {
                             child: TextFormField(
                               decoration: InputDecoration(
                                 enabledBorder: OutlineInputBorder(
-                                  borderSide:BorderSide(color: Color(0xFFF73D99)),
+                                  borderSide:
+                                      BorderSide(color: Color(0xFFF73D99)),
                                   borderRadius: BorderRadius.circular(15.0),
                                 ),
                                 focusedBorder: OutlineInputBorder(
@@ -116,7 +164,8 @@ class _SigninFormState extends State<SigninForm> {
                               controller: passwordController,
                               decoration: InputDecoration(
                                 enabledBorder: OutlineInputBorder(
-                                  borderSide:BorderSide(color: Color(0xFF61C6C0)),
+                                  borderSide:
+                                      BorderSide(color: Color(0xFF61C6C0)),
                                   borderRadius: BorderRadius.circular(15.0),
                                 ),
                                 focusedBorder: OutlineInputBorder(
@@ -150,7 +199,8 @@ class _SigninFormState extends State<SigninForm> {
                               controller: confirmPasswordController,
                               decoration: InputDecoration(
                                 enabledBorder: OutlineInputBorder(
-                                  borderSide:BorderSide(color: Color(0xFFBDCC11)),
+                                  borderSide:
+                                      BorderSide(color: Color(0xFFBDCC11)),
                                   borderRadius: BorderRadius.circular(15.0),
                                 ),
                                 focusedBorder: OutlineInputBorder(
@@ -186,8 +236,11 @@ class _SigninFormState extends State<SigninForm> {
                                     Scaffold.of(context).showSnackBar(SnackBar(
                                         content: Text('Processing Data')));
                                     _formkey.currentState.save();
+                                    print(username + email + password);
+                                    _signup("Terry", username, email, password,
+                                        confirmPassword);
                                     // print(email);
-                                    // print(userVname);
+                                    // print(username);
                                     // print(password);
                                     // print(confirmPassword);
                                   }
