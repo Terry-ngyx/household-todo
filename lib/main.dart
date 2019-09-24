6cdb8e92318cd1340ef93ec16e6b37f0587d744f
linglee <lingleechai>
2019-09-24 12:32:40 +0800

    Added Splash Screen for the app, ICON change

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ import 'session/profile.dart';
 import 'session/profileedit.dart';
 import 'session/schedulepage.dart';
 
-const Home = '/';
+const Home = '/welcome';
 const LoginRoute = '/login';
 const SignUpRoute = '/signup';
 const HomeRoute = '/home';
@@ -39,8 +39,7 @@ class MyApp extends StatefulWidget {
   // This widget is the root of your application.
 }
 
-class _MyAppState extends State<MyApp>{
-
+class _MyAppState extends State<MyApp> {
   Timer _timer;
   List<Message> messages = [];
 
@@ -48,42 +47,42 @@ class _MyAppState extends State<MyApp>{
   void initState() {
     super.initState();
     print("hello");
-    
+
     firebaseMessaging.configure(
       onMessage: (Map<String, dynamic> message) async {
-        if (message['data'].title == "There's a grocery shop nearby! Do your work!"){
+        if (message['data'].title ==
+            "There's a grocery shop nearby! Do your work!") {
           print("Got Message");
           stopTimer();
         }
         print("onMessage: $message");
         final notification = message['notification'];
         setState(() {
-          messages.add(Message(
-              notification['title'], notification['body']));
+          messages.add(Message(notification['title'], notification['body']));
         });
       },
       onLaunch: (Map<String, dynamic> message) async {
-        if (message['data'].title == "There's a grocery shop nearby! Do your work!"){
+        if (message['data'].title ==
+            "There's a grocery shop nearby! Do your work!") {
           print("Got Message");
           stopTimer();
         }
         print("onMessage: $message");
         final notification = message['notification'];
         setState(() {
-          messages.add(Message(
-              notification['title'], notification['body']));
+          messages.add(Message(notification['title'], notification['body']));
         });
       },
       onResume: (Map<String, dynamic> message) async {
-        if (message['data'].title == "There's a grocery shop nearby! Do your work!"){
+        if (message['data'].title ==
+            "There's a grocery shop nearby! Do your work!") {
           print("Got Message");
           stopTimer();
         }
         print("onMessage: $message");
         final notification = message['notification'];
         setState(() {
-          messages.add(Message(
-              notification['title'], notification['body']));
+          messages.add(Message(notification['title'], notification['body']));
         });
       },
     );
@@ -93,7 +92,7 @@ class _MyAppState extends State<MyApp>{
   }
 
   void startTimer() {
-    setState(() {  
+    setState(() {
       _timer = Timer.periodic(Duration(minutes: 15), (timer) {
         getCurrentLocation();
       });
@@ -107,25 +106,27 @@ class _MyAppState extends State<MyApp>{
 
   getCurrentLocation() async {
     FlutterBackgroundLocation().getCurrentLocation().then((location) async {
-      print("This is current Location " + location.longitude.toString() + " " + location.latitude.toString());
+      print("This is current Location " +
+          location.longitude.toString() +
+          " " +
+          location.latitude.toString());
       String token = await storage.read(key: 'jwt');
       String url = 'http://10.0.2.2:5000/api/v1/users/completepublictask';
-      String json = '{"latitude": "$location.latitude.toString()", "longitude": "$location.longtitude.toString()"}';
-      http.Response response = await http.post(
-        url,
-        headers: {
-          'Content-type': 'application/json',
-          'Authorization':'Bearer $token'
-        },
-        body: json
-      );
+      String json =
+          '{"latitude": "$location.latitude.toString()", "longitude": "$location.longtitude.toString()"}';
+      http.Response response = await http.post(url,
+          headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer $token'
+          },
+          body: json);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Household',
+      title: 'Chores of Duty',
       theme: ThemeData(
           pageTransitionsTheme: PageTransitionsTheme(builders: {
             TargetPlatform.android: CupertinoPageTransitionsBuilder(),
@@ -138,7 +139,8 @@ class _MyAppState extends State<MyApp>{
               .textTheme
               .apply(bodyColor: Colors.white, displayColor: Colors.white)),
       onGenerateRoute: Router.generateRoute,
-      initialRoute: Home,
+      home: SplashScreen(),
+      // initialRoute: Home,
     );
   }
 }
@@ -171,5 +173,88 @@ class Router {
                 body: Center(
                     child: Text('No route defined for ${settings.name}'))));
     }
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = Duration(seconds: 4);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed("/welcome");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print("Splash Screen trii");
+    startTime();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Color(0xFFABC8FE)),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 3,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.accessibility_new,
+                          color: Color(0xFF234875),
+                          size: 90.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                      ),
+                      Text(
+                        "Don't say I Bo-Chore",
+                        style: SplashScreenText
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(
+                      backgroundColor: Colors.white,
+                      valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF234875)),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0,20.0,0,20.0),
+                    ),
+                    Text("Git your chores done !", textAlign: TextAlign.center, style: SplashScreenSmallText,)
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
